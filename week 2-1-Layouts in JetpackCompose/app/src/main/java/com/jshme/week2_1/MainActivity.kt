c371a613b325e3b98617ee6f887c3502dce5881c
jsh-me <ppm_it@naver.com>
2021-11-15 23:43:09 +0900

    week2-1 study measurable, constraint

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/jshme/week2_1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/jshme/week2_1/MainActivity.kt
@@ -19,9 +19,10 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.jshme.week2_1.ui.theme.Week21Theme
@@ -62,15 +63,15 @@ fun LayoutCodeLab() {
     }
 }
 
-@Composable
-fun BodyContent(modifier: Modifier) {
-    Column(
-        modifier = modifier
-    ) {
-        Text(text = "thanks for going through the layouts codelab")
-        Text(text = "hi there!!")
-    }
-}
+//@Composable
+//fun BodyContent(modifier: Modifier) {
+//    Column(
+//        modifier = modifier
+//    ) {
+//        Text(text = "thanks for going through the layouts codelab")
+//        Text(text = "hi there!!")
+//    }
+//}
 
 @Preview(showBackground = true)
 @Composable
@@ -188,5 +189,86 @@ fun ImageList(scrollState: LazyListState) {
         items(100) {
             ImageListItem(index = it)
         }
+    }
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        // Composable 이 first baseline 을 가지고 있는지 판단
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        // height of composable with padding - first baseline
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    Week21Theme {
+        Text("Hi there!!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    Week21Theme {
+        Text("Hi there!!", Modifier.padding(top = 32.dp))
+    }
+}
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        // 주어진 constraints 들만을 이용해서 측정해라.
+        // 측정된 자식 리스트들
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+
+        var yPosition = 0
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            placeables.forEach {placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
+    }
+}
+
+@Preview
+@Composable
+fun MyOwnColumnPaddingPreview() {
+    Week21Theme {
+        BodyContent()
     }
 }
