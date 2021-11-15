1320c1fb38922d82b38215eda25f78fa46904a09
jsh-me <ppm_it@naver.com>
2021-11-15 23:17:05 +0900

    week2-1 add scroll state

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/jshme/week2_1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/jshme/week2_1/MainActivity.kt
@@ -3,12 +3,11 @@ package com.jshme.week2_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -16,6 +15,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,7 +23,10 @@ import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.jshme.week2_1.ui.theme.Week21Theme
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +48,7 @@ fun LayoutCodeLab() {
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text ="sehee jeong")
+                    Text(text = "sehee jeong")
                 },
                 actions = {
                     IconButton(onClick = {}) {
@@ -79,14 +82,14 @@ fun DefaultPreview() {
 
 @Composable
 fun PhotographerCard() {
-    Row (
+    Row(
         modifier = Modifier
             .padding(16.dp)
             .clip(RoundedCornerShape(4.dp))
             .background(MaterialTheme.colors.surface)
             .clickable { }
             .padding(16.dp)
-        ){
+    ) {
         Surface(
             modifier = Modifier.size(50.dp),
             shape = CircleShape,
@@ -113,6 +116,77 @@ fun PhotographerCard() {
 @Composable
 fun PhotographerCardPreview() {
     Week21Theme {
-        PhotographerCard()
+        initViews()
+    }
+}
+
+@Composable
+fun initViews() {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+
+
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text(
+                    "Scroll to the Top"
+                )
+            }
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text(
+                    "Scroll to the end"
+                )
+            }
+        }
+
+        ImageList(scrollState)
+
+    }
+}
+
+@Composable
+fun SimpleList() {
+    // xml에 scrollview 를 안달아주면, 스크롤이 안되는 것처럼
+    // 컴포즈도 동일하다.
+    val scrollState = rememberLazyListState()
+
+    LazyColumn(state = scrollState) {
+        items(100) {
+            Text("Item #$it")
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"),
+            contentDescription = "Android logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text("Item $index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun ImageList(scrollState: LazyListState) {
+    LazyColumn(state = scrollState) {
+        items(100) {
+            ImageListItem(index = it)
+        }
     }
 }
